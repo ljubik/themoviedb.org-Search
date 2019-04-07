81ea1307fc1f6a6602d3913d302b38c65e707f53
Liubomyr Kokor <23187063+ljubik@users.noreply.github.com>
2019-04-07 16:37:29 +0300

    make load list and try make link to logo

--- js/js.js
+++ js/js.js
@@ -5,5 +5,32 @@ var total_paginas = "";
 $(document).ready(function() {
   urlApi = urlApi + apiKey;
   cargarTabla(urlApi, 1);
-  $("#flecha-izda").addClass("oculto");
+});
+
+var cargarTabla = function(urlApiApiKey, numero) {
+  urlApiApiKey += "&page=" + numero;
+  $.get(urlApiApiKey, function(respuesta, estado) {
+    if (estado === "success") {
+      total_paginas = respuesta.total_pages;
+      var Films = "<div>";
+
+      $.each(respuesta.results, function(indice, elemento) {
+        Films += "<div>";
+        Films +=
+          '<br class="title"><a href="">' + elemento.title + "</a></div>";
+      });
+    } else {
+      Films = "<div><p>Фільми недоступні</p></div>";
+    }
+    document.getElementById("principal").innerHTML = Films + "</div>";
+  });
+};
+
+$("#btnFind").click(function() {
+  var busqueda = document.getElementById("busqueda").value;
+  urlApiBusqueda =
+    "https://api.themoviedb.org/3/search/movie?include_adult=false&page=1&query=" +
+    busqueda +
+    "&language=en-US&api_key=c2406e33bae3c04a8fdebb618c81ede7";
+  cargarTabla(urlApiBusqueda, 1);
 });
