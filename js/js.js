f573d1dd5bfe9d07cc6ba061021cb923e2553db5
Liubomyr Kokor <23187063+ljubik@users.noreply.github.com>
2019-04-07 16:09:05 +0300

    add js.file

---
+++ js/js.js
@@ -0,0 +1,9 @@
+﻿var urlApi = "https://api.themoviedb.org/3/search/movie?";
+var apiKey = "api_key=c2406e33bae3c04a8fdebb618c81ede7";
+var total_paginas = "";
+
+$(document).ready(function() {
+  urlApi = urlApi + apiKey;
+  cargarTabla(urlApi, 1);
+  $("#flecha-izda").addClass("oculto");
+});
